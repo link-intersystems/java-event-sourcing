59377ea9f2742a148884f0032f6a12955efdbc9d
René Link <rene.link@link-intersystems.com>
2023-10-13 11:23:16 +0200

    Request and response model improved.

--- inventory-management/use-cases/src/main/java/com/link_intersystems/inventory/InventoryItemReceivedUseCase.java
+++ inventory-management/use-cases/src/main/java/com/link_intersystems/inventory/InventoryItemReceivedUseCase.java
@@ -1,12 +1,22 @@
 package com.link_intersystems.inventory;
 
+import static java.util.Objects.*;
+
 public interface InventoryItemReceivedUseCase {
 
     public ResponseModel itemsReceived(RequestModel requestModel);
 
     public static record RequestModel(String itemIdentifier, int receivedQuantity) {
+
+        public RequestModel {
+            requireNonNull(itemIdentifier);
+        }
     }
 
     public static record ResponseModel(String itemIdentifier, int actualQuantity) {
+
+        public ResponseModel {
+            requireNonNull(itemIdentifier);
+        }
     }
 }
